54a404a68647ce60c1656d378eb30a16708fb28c
Walber Jefferson <walberjefferson@hotmail.com>
2019-10-31 14:40:13 -0300

    Ajuste na altura do slider.

--- template-parts/content/lista-carrosel.php
+++ template-parts/content/lista-carrosel.php
@@ -15,15 +15,15 @@ $query_sticks = new WP_Query($args);
                 <?php if (image_destaque_field('imagem_slide')): ?>
                     <li>
                         <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                            <?= image_destaque_field('imagem_slide', 1350, 350, ['class' => 'img-fluid d-none d-md-block']); ?>
-                            <?= image_destaque_field('imagem_slide', 800, 400, ['class' => 'img-fluid d-block d-md-none']); ?>
+                            <?= image_destaque_field('imagem_slide', 1350, 260, ['class' => 'img-fluid d-none d-md-block']); ?>
+                            <?= image_destaque_field('imagem_slide', 800, 300, ['class' => 'img-fluid d-block d-md-none']); ?>
                         </a>
                     </li>
                 <?php elseif (image_destaque()) : ?>
                     <li>
                         <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                            <?= image_destaque(1350, 350, ['class' => 'img-fluid d-none d-md-block']); ?>
-                            <?= image_destaque(800, 400, ['class' => 'img-fluid d-block d-md-none']); ?>
+                            <?= image_destaque(1350, 260, ['class' => 'img-fluid d-none d-md-block']); ?>
+                            <?= image_destaque(800, 300, ['class' => 'img-fluid d-block d-md-none']); ?>
                         </a>
                     </li>
                 <?php endif; ?>
